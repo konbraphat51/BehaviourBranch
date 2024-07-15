b048f0bca71c8869b67bd76b83caec5e9478170c
konbraphat51 <konbraphat@gmail.com>
2024-07-16 01:26:45 +0900

    add comment

--- Unity_Client/BehaviourBranchAgent.cs
+++ Unity_Client/BehaviourBranchAgent.cs
@@ -4,6 +4,9 @@ namespace BehaviourBranch
 {
     public abstract class BehaviourBranchAgent
     {
+        /// <summary>
+        /// Implement this method to convert variable name to Variable Node
+        /// </summary>
         public abstract float ConvertVariable(string variableName);
 
         public abstract BehaviourRunner CreateInstance(
